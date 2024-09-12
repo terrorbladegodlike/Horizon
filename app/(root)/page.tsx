398d4d9d8bbbee848b182e852ca271f856ff67bc
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-09-12 22:12:02 +0300

    Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,30 @@
+// Import Components
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
 const Home = () => {
-   return <div>Home</div>
+   const loggedIn = { firstName: 'Mihail' }
+
+   return (
+      <section className='home'>
+         <div className='home-content'>
+            <header className='home-header'>
+               <HeaderBox
+                  type='greeting'
+                  title='Welcome'
+                  user={loggedIn?.firstName || 'Guest'}
+                  subtext='Access and manage your account and transactions efficiently.'
+               />
+
+               <TotalBalanceBox
+                  accounts={[]}
+                  totalBanks={1}
+                  totalCurrentBalance={1250.58}
+               />
+            </header>
+         </div>
+      </section>
+   )
 }
 
 export default Home
